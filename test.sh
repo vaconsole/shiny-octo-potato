f787305e025d36e3a55fb3830ea3b588cbb406b4
Marvin Wu <marvin.wu@urbanspring.hk>
2021-05-06 12:06:00 +0800

    test

---
+++ test.sh
@@ -0,0 +1,9 @@
+wget \
+     --recursive \
+     --html-extension \
+     --restrict-file-names=unix \
+     --domains succulentsandsunshine.com \
+     --random-wait \
+     --limit-rate=200k \
+     --user-agent=Mozilla \
+     --no-parent  https://www.succulentsandsunshine.com/
