1531b9ab0e29207bb91a3faac4d9026d09e4e555
Marvin Wu <marvin.wu@urbanspring.hk>
2021-05-07 10:03:14 +0800

    test

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const scrape = require('website-scraper')
 const fs = require('fs')
 var stringify = require('json-stringify-safe');
 const SaveToExistingDirectoryPlugin = require('website-scraper-existing-directory');
+const {urlTypeFilter} = require('./util')
 
-const targetUrl = 'https://www.succulentsandsunshine.com'
+
+const targetUrl = 'https://bbu.hk/'
 
 const options = {
   urls: [targetUrl],
-  urlFilter: function(url) {
-    return url.indexOf(targetUrl) === 0;
-  },
+  urlFilter: (url)=>urlTypeFilter(url,targetUrl),
   request: {
     headers: {
       'User-Agent': 'Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 4 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19'
@@ -20,10 +20,11 @@ const options = {
   filenameGenerator: 'bySiteStructure',
   directory: './site',
   ignoreErrors : true,
-  requestConcurrency: 4,
   plugins: [ new SaveToExistingDirectoryPlugin() ]
 }
 
 // with async/await
-scrape(options).then((result) => {fs.writeFileSync('./site/index.json',stringify(result))})
+scrape(options).then((result) => {fs.writeFileSync('./site/index.json',stringify(result))
+console.log('index json exported')
+})
 
